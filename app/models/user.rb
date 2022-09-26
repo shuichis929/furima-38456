e3422ce8db88ce583e14c548544c3fa8b9854f60
shuichis929 <shuichis929@gmail.com>
2022-09-27 00:11:03 +0900

    userモデルの単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,11 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
+
   validates :nickname, presence: true
   validates :family_name, presence: true
   validates :first_name, presence: true
   validates :family_name_kana, presence: true
   validates :first_name_kana, presence: true
   validates :birthday, presence: true
+
+  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+  validates :password, format: { with: VALID_PASSWORD_REGEX }
+
+  VALID_NAME_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  validates :family_name, format: { with: VALID_NAME_REGEX }
+  validates :first_name, format: { with: VALID_NAME_REGEX }
+
+  VALID_NAME_KANA_REGEX = /\A[ァ-ヶー－]+\z/
+  validates :family_name_kana, format: { with: VALID_NAME_KANA_REGEX }
+  validates :first_name_kana, format: { with: VALID_NAME_KANA_REGEX }
 end
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,10 +3,10 @@ FactoryBot.define do
     transient do
       person { Gimei.name }
     end
-    nickname {Faker::Name.name}
-    email {Faker::Internet.free_email}
-    password {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    nickname { Faker::Name.name }
+    email { Faker::Internet.free_email }
+    password { Faker::Internet.password(min_length: 6) }
+    password_confirmation { password }
     family_name { person.last.kanji }
     first_name { person.first.kanji }
     family_name_kana { person.last.katakana }
