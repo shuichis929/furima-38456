896a25786a779f6f0ea754444bda45f8dce1fc9a
shuichis929 <shuichis929@gmail.com>
2022-09-28 16:14:01 +0900

    itemモデルのバリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -6,6 +6,16 @@ class Item < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :delivery_charge
-  belongs_to :days_to_ship
   belongs_to :prefecture
+  belongs_to :days_to_ship
+  
+  validates :item_name, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :condition_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :delivery_charge_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"} 
+  validates :days_to_ship_id, numericality: { other_than: 1 , message: "can't be blank"} 
 end
