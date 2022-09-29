5562f90293cbafbe657f6d103e41f0016ae978e1
shuichis929 <shuichis929@gmail.com>
2022-09-29 19:45:06 +0900

    itemモデルのバリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -14,7 +14,6 @@ class Item < ApplicationRecord
   validates :description, presence: true
   validates :price, presence: true
   validates :price, numericality: { only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 },
-                    format: { with: /\A[0-9]+\z/ }
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
