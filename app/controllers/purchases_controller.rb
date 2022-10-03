35f1a35ac62f8549075b29eefdebc4d962632759
shuichis929 <shuichis929@gmail.com>
2022-10-03 18:43:32 +0900

    購入ページで入力不備があった場合のエラーメッセージを表示させる実装

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,15 +1,23 @@
 class PurchasesController < ApplicationController
   def index
     @item = Item.find(params[:item_id])
+    @purchase_shipping_address = PurchaseShippingAddress.new
   end
 
   def create
     @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
+    if @purchase_shipping_address.valid?
+      @purchase_shipping_address.save
+      redirect_to root_path
+    else
+      @item = Item.find(params[:item_id])
+      render 'purchases/index'
+    end
   end
 
   private
 
   def purchase_params
-    params.require(:purchase_shipping_address).merge(user_id: current_user.id, item_id: item[:id])
+    params.require(:purchase_shipping_address).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id])
   end
 end
--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -2,20 +2,20 @@ class PurchaseShippingAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number, :purchase_id
 
-  with_option presence: true do
+  with_options presence: true do
     validates :user_id
     validates :item_id
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
     validates :city
     validates :block
     validates :phone_number, format: {with: /[0-9]{10,11}/}
-    validates :purchase_id
   end
-  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
+  
 
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
 
-    ShippingAddress.create(post_code :post_code, prefecture_id :prefecture_id, city :city, block :block, building :building, phone_number :phone_number, purchase_id :purchase_id)
+    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase_id)
   end
 end
