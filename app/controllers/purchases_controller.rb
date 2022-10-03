f9db976db792e67aea9f5d0f78c6ef477cedbc4d
shuichis929 <shuichis929@gmail.com>
2022-10-03 16:48:27 +0900

    indexアクションのビューおよびコントローラー内の記述変更

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,6 +1,6 @@
 class PurchasesController < ApplicationController
   def index
-    @purchase_shipping_address = PurchaseShippingAddress.new
+    @item = Item.find(params[:item_id])
   end
 
   def create
