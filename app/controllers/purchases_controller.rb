c144a4909d304be3ed73fecc833cd6177ba98fd8
shuichis929 <shuichis929@gmail.com>
2022-10-03 15:13:52 +0900

    purchasesコントローラーのアクション設定

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,2 +1,15 @@
 class PurchasesController < ApplicationController
+  def index
+    @purchase_shipping_address = PurchaseShippingAddress.new
+  end
+
+  def create
+    @purchase_shipping_address = PurchaseShippingAddress.new(purchase_params)
+  end
+
+  private
+
+  def purchase_params
+    params.require(:purchase_shipping_address).merge(user_id: current_user.id, item_id: item[:id])
+  end
 end
