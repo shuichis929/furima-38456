83a2d6cedfbf6d6e542e9b69a4f09ea77dfcf4ab
shuichis929 <shuichis929@gmail.com>
2022-10-03 14:51:40 +0900

    バリデーションの設定

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -1,4 +1,15 @@
 class PurchaseShippingAddress
   include ActiveModel::Model
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number, :purchase_id
+
+  with_option presence: true do
+    validates :user_id
+    validates :item_id
+    validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :city
+    validates :block
+    validates :phone_number, format: {with: /[0-9]{10,11}/}
+    validates :purchase_id
+  end
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end
