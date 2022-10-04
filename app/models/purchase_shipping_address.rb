1ec4d902c23e08685bc3390f31b627c540e1c836
shuichis929 <shuichis929@gmail.com>
2022-10-04 15:09:30 +0900

    shipping_addressesテーブルに保存するための記述の変更

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -17,6 +17,6 @@ class PurchaseShippingAddress
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
 
-    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase_id)
+    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
