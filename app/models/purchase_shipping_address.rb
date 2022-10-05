9b558b51f583be5970b1bc5e83bbe1a55b2ddc68
shuichis929 <shuichis929@gmail.com>
2022-10-05 15:12:53 +0900

    バリデーションの追加

--- app/models/purchase_shipping_address.rb
+++ app/models/purchase_shipping_address.rb
@@ -3,6 +3,8 @@ class PurchaseShippingAddress
   attr_accessor :user_id, :item_id, :post_code, :prefecture_id, :city, :block, :building, :phone_number, :purchase_id, :token
 
   with_options presence: true do
+    validates :user_id
+    validates :item_id
     validates :token
     validates :post_code, format: { with: /\A[0-9]{3}-[0-9]{4}\z/, message: 'is invalid. Include hyphen(-)' }
     validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
@@ -14,7 +16,6 @@ class PurchaseShippingAddress
   def save
     purchase = Purchase.create(user_id: user_id, item_id: item_id)
 
-    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building,
-                           phone_number: phone_number, purchase_id: purchase.id)
+    ShippingAddress.create(post_code: post_code, prefecture_id: prefecture_id, city: city, block: block, building: building, phone_number: phone_number, purchase_id: purchase.id)
   end
 end
