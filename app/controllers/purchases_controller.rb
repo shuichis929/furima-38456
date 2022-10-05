5d5b99e7476e1288f265251043021a0420bb48b9
shuichis929 <shuichis929@gmail.com>
2022-10-05 12:36:11 +0900

    Rubocopの実施

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -23,11 +23,13 @@ class PurchasesController < ApplicationController
   private
 
   def purchase_params
-    params.require(:purchase_shipping_address).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+    params.require(:purchase_shipping_address).permit(:post_code, :prefecture_id, :city, :block, :building, :phone_number).merge(
+      user_id: current_user.id, item_id: params[:item_id], token: params[:token]
+    )
   end
 
   def pay_item
-    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
     Payjp::Charge.create(
       amount: @item.price,
       card: purchase_params[:token],
--- spec/factories/purchase_shipping_addresses.rb
+++ spec/factories/purchase_shipping_addresses.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :purchase_shipping_address do
-
     token { 'tok_abcdefghijk00000000000000000' }
     post_code { '123-4567' }
     prefecture_id { '48' }
@@ -8,7 +7,5 @@ FactoryBot.define do
     block { '緑区' }
     building { '柳ビル103' }
     phone_number { '09012345678' }
-    
-
   end
 end
