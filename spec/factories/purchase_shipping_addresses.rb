ae9aeb95d6740820741daa0d42d512d9fad680ad
shuichis929 <shuichis929@gmail.com>
2022-10-05 12:25:38 +0900

    formオブジェクトのモデルの単体テストの実施

---
+++ spec/factories/purchase_shipping_addresses.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :purchase_shipping_address do
+
+    token { 'tok_abcdefghijk00000000000000000' }
+    post_code { '123-4567' }
+    prefecture_id { '48' }
+    city { '横浜市' }
+    block { '緑区' }
+    building { '柳ビル103' }
+    phone_number { '09012345678' }
+    
+
+  end
+end
